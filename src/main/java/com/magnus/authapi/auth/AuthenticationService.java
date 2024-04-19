6b2e6a786253bbda0827e0fd54e957b0c5a3f4df
Magnus Paal <magnuspaal@gmail.com>
2024-04-19 15:53:59 +0300

    feat: keep refresh token active after invalidation
    - keep refresh token valid for 2 minutes after a new access token is requested
    - log refresh token steps
    - add base entity with created_at, updated_at and deleted_at

--- src/main/java/com/magnus/authapi/auth/AuthenticationService.java
+++ src/main/java/com/magnus/authapi/auth/AuthenticationService.java
@@ -18,6 +18,8 @@ import com.magnus.authapi.user.UserRole;
 import com.magnus.authapi.security.config.JwtService;
 import com.magnus.authapi.utils.DateUtils;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -26,12 +28,15 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
+
+  Logger logger = LoggerFactory.getLogger(AuthenticationService.class);
 
   private final AuthenticationManager authenticationManager;
   private final PasswordEncoder passwordEncoder;
@@ -43,7 +48,7 @@ public class AuthenticationService {
   @Autowired
   private final ApiProperties apiProperties;
 
-  private final Integer tokenExpiry = 1000 * 60 * 5; // 5 minutes
+  private final Integer tokenExpiry = 1000 * 20; // 5 minutes
 
   public AuthenticationResponse register(RegistrationRequest request) throws EmailAlreadyTakenException, UsernameAlreadyTakenException {
     String email = request.getEmail().toLowerCase();
@@ -110,24 +115,42 @@ public class AuthenticationService {
         .build();
   }
 
-  public AuthenticationResponse refreshToken(RefreshTokenRequest request) throws InvalidRefreshTokenException {
-
+  public synchronized AuthenticationResponse refreshToken(RefreshTokenRequest request) throws InvalidRefreshTokenException {
     String refreshToken = request.getRefreshToken();
     String userEmail = jwtService.extractUsername(refreshToken);
     User user = userRepository.findByEmail(userEmail).orElseThrow();
     UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
 
+    logger.info( "\u001B[32mrequested new access token\u001B[0m [ " + userEmail + " ]");
+
     if (jwtService.isTokenValid(refreshToken, userDetails)) {
       UserToken userToken = userTokenRepository.findByUserAndValidAndToken(user, true, refreshToken).orElse(null);
-      if (userToken != null && userToken.isValid()) {
+
+      if (dbRefreshTokenIsValid(userToken)) {
+        logger.info("\u001B[32muser token from db\u001B[0m [ " + userEmail + " ], [ " + userToken.getToken() + " ]");
+
         Date expiresAt = new Date(System.currentTimeMillis() + tokenExpiry);
         invalidateRefreshToken(userToken.getUser(), userToken.getToken());
         String jwtToken = jwtService.generateToken(userToken.getUser(), expiresAt);
         String newRefreshToken = this.generateAndSaveRefreshToken(userToken.getUser());
+
+        logger.info("\u001B[32mreturn refresh token\u001B[0m [ " + userEmail + " ], [ " + newRefreshToken + " ]");
         return AuthenticationResponse.builder().token(jwtToken).refreshToken(newRefreshToken).expiresAt(DateUtils.getDateISO8601GMTString(expiresAt)).build();
       }
     }
+
+    logger.info("\u001B[31minvalid refresh token\u001B[0m [ " + userEmail + " ], [ " + refreshToken + " ]");
     throw new InvalidRefreshTokenException();
+  }
+
+  private boolean dbRefreshTokenIsValid(UserToken userToken) {
+    boolean notNull = userToken != null;
+    if (notNull) {
+      boolean valid = userToken.isValid();
+      boolean notExpired = userToken.getExpiryDate() == null || LocalDateTime.now().isBefore(userToken.getExpiryDate());
+      return valid && notExpired;
+    }
+    return false;
   }
 
   public void confirmUser(String token) {
@@ -143,12 +166,21 @@ public class AuthenticationService {
 
   private String generateAndSaveRefreshToken(User user) {
     String refreshToken = jwtService.generateRefreshToken(user);
-    invalidateRefreshToken(user, refreshToken);
+    deleteRefreshToken(user, refreshToken);
     userTokenRepository.save(new UserToken(user, TokenType.REFRESH_TOKEN, refreshToken));
     return refreshToken;
   }
 
   private void invalidateRefreshToken(User user, String token) {
+    userTokenRepository.findByUserAndValidAndToken(user, true, token).ifPresent((presentToken) -> {
+      if (presentToken.getExpiryDate() == null) {
+        presentToken.setExpiryDate(LocalDateTime.now().plusMinutes(2));
+        userTokenRepository.save(presentToken);
+      }
+    });
+  }
+
+  private void deleteRefreshToken(User user, String token) {
     userTokenRepository.findByUserAndValidAndToken(user, true, token).ifPresent(userTokenRepository::delete);
   }
 }
--- src/main/java/com/magnus/authapi/token/UserToken.java
+++ src/main/java/com/magnus/authapi/token/UserToken.java
@@ -1,16 +1,19 @@
 package com.magnus.authapi.token;
 
+import com.magnus.authapi.common.BaseEntity;
 import com.magnus.authapi.user.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @Entity
-public class UserToken {
+public class UserToken extends BaseEntity {
   @Id
   @SequenceGenerator(
       name = "token_sequence",
@@ -22,14 +25,20 @@ public class UserToken {
       generator = "token_sequence"
   )
   private Long id;
+
   @ManyToOne()
   @JoinColumn(name="user_id")
   private User user;
+
   @Enumerated(EnumType.STRING)
   private TokenType tokenType;
+
   @Column(length = 512)
   private String token;
+
   private boolean valid = true;
+
+  private LocalDateTime expiryDate;
 
   public UserToken(User user, TokenType tokenType, String token) {
     this.user = user;
